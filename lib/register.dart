b5652057f659000db7bc226324541b45a53bb177
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-17 21:52:33 +0545

    added user register page

--- lib/register.dart
+++ lib/register.dart
@@ -10,6 +10,106 @@ class userRegister extends StatefulWidget {
 class _userRegisterState extends State<userRegister> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/register.png")
+        )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 150, left: 40),
+              child: Text("User Registration", style: TextStyle(
+                color: Colors.white, fontSize: 34, fontWeight: FontWeight.bold
+              ),),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.37, left: 40, right: 40),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your Name",
+                        fillColor: Colors.grey.shade200,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your Number",
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your Email",
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter your Password",
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text("Register", style: TextStyle(
+                          fontSize: 27, color: Colors.white, fontWeight: FontWeight.w700,
+                        ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        TextButton(onPressed: (){
+                          Navigator.pushNamed(context, 'login');
+                          }, child: Text("Already have account? login here", style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.white,
+                          fontSize: 18
+                        ),))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]
+        ),
+      ),
+    );
   }
 }
